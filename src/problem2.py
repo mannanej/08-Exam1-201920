58e260a14e84bb967bd816a8712f891c2b986b32
mannanej <mannanej@rose-hulman.edu>
2018-12-11 20:44:49 -0500

    Done

--- src/problem2.py
+++ src/problem2.py
@@ -3,8 +3,8 @@ Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Eddie Mannan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -44,6 +44,41 @@ def test_factor_sum():
     ###########################################################################
     # WRITE YOUR TESTS BELOW HERE:
     ###########################################################################
+
+    #Test 1:
+    expected = 6
+    actual = factor_sum(5)
+    print('Test 1:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 2:
+    expected = 9
+    actual = factor_sum(10)
+    print('Test 2:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 3:
+    expected = 6
+    actual = factor_sum(15)
+    print('Test 3:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 4:
+    expected = 6
+    actual = factor_sum(20)
+    print('Test 4:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    # Test 5:
+    expected = 4
+    actual = factor_sum(25)
+    print('Test 5:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
 
 
 def factor_sum(n):
